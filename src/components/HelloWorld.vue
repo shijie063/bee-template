899cb63cc979d3f44183a34a182d51fc9b1e5117
“石杰“ <shijie@cdvcloud.com>
2021-08-04 18:43:03 +0800

    fix: 代码格式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,22 @@
 <template>
-  <h1>HelloWorld Bee-template</h1>
-  <img src="../assets/logo.png" alt="">
+    <h1>Hello Bee-template</h1>
+    <img src="../assets/logo.png" alt="" />
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
+    name: 'HelloWorld',
+    props: {
+            msg: {
+                type: String,
+                required: true,
+            },
+        },
+    setup: () => {
+        const count = ref(0)
+        return { count }
+    },
 })
 </script>
 
